9bcfa61b15046bd805ef55ceb8a6bac6805ac53b
praveenkondani <praveenaprdc@gmail.com>
2018-04-18 13:09:13 +0530

    d3 check

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,23 @@ var showSpinner = function(){
 }
 var hideSpinner = function(){
     $("#spinnerLoad").css("display", "none");
-    
 }
+$(".processData").css("display", "none");
 setTimeout(hideSpinner,2000);
+
+$(".hero-box").click(function(){
+    $(".hero-box").removeClass("active-box");
+    $(this).addClass("active-box");
+})
+$(".hero-box:nth-child(1)").click(function(){
+    $(".dataBox").css("display", "none")
+    $(".dataBox:nth-child(1)").css("display", "block")
+})
+$(".hero-box:nth-child(2)").click(function(){
+    $(".dataBox").css("display", "none")
+    $(".dataBox:nth-child(2)").css("display", "block")
+})
+$(".hero-box:nth-child(3)").click(function(){
+    $(".dataBox").css("display", "none")
+    $(".dataBox:nth-child(3)").css("display", "block")
+})
